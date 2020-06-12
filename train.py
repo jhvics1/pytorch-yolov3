1c2a70d392e4125648f471f602e435028b5b409f
Jay Oh <jhvics1@gmail.com>
2020-06-13 00:10:32 +0900

    add arguments
    - iter_to_save
    - augmentation
    
    Signed-off-by: Jay Oh <jhvics1@gmail.com>

--- train.py
+++ train.py
@@ -10,10 +10,11 @@ from utils.parse_config import *
 from test import evaluate
 
 
-def train():
+def train(args):
     global metrics
     # Get dataloader
-    dataset = ListDataset(train_path, img_size=args.img_size, augment=True, multiscale=args.multiscale_training)
+    dataset = ListDataset(train_path, img_size=args.img_size, augment=args.augmentation,
+                          multiscale=args.multiscale_training)
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=args.batch_size,
                                              shuffle=True,
@@ -92,13 +93,16 @@ def train():
         logger.list_of_scalars_summary(evaluation_metrics, epoch)
 
         # Save checkpoint file
-        torch.save(model.state_dict(), "checkpoints/yolov3_ckpt_{}.pth".format(epoch + 1))
+        if (epoch + 1) % args.iter_to_save == 0:
+            print("Saving Checkpoint file after [{}] iteration".format(str(epoch + 1)))
+            torch.save(model.state_dict(), "checkpoints/yolov3_ckpt_{}.pth".format(epoch + 1))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=200, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
+    parser.add_argument('--augmentation', action='store_true', default=False)
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/yolov3-voc.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/voc.data", help="path to data config file")
@@ -107,6 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
     parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    parser.add_argument("--iter_to_save", type=int, default=10, help="save checkpoint.pth every given iter.")
     parser.add_argument("--dev_id", default=0, help="GPU device id. Default 0")
     args = parser.parse_args()
     print(args)
@@ -148,4 +153,4 @@ if __name__ == "__main__":
         else:
             model.load_darknet_weights(args.pretrained_weight)
 
-    train()
+    train(args)
