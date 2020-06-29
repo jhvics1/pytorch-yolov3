fa16b0bcdce87429b4a73e9e736f9a6c4ec3d173
Jay Oh <jhvics1@gmail.com>
2020-06-29 14:16:26 +0900

    bug fix - transpose array sequence when augmenting given image
    Signed-off-by: Jay Oh <jhvics1@gmail.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -43,6 +43,7 @@ def random_affine(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10,
     # https://medium.com/uruvideo/dataset-augmentation-with-random-homographies-a8f4b44830d4
     # targets = [cls, xyxy]
     img = img.numpy()
+    img = img.transpose((1, 2, 0))
     targets = targets.numpy()
 
     height = img.shape[0] + border * 2
@@ -106,28 +107,39 @@ def random_affine(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10,
         targets = targets[i]
         targets[:, 1:5] = xy[i]
 
+        img = img.transpose((2, 0, 1))
         img = torch.from_numpy(img)
         targets = torch.from_numpy(targets)
     return img, targets
 
 
-def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
-    r = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
-    hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
-    dtype = img.dtype  # uint8
-
-    x = np.arange(0, 256, dtype=np.int16)
-    lut_hue = ((x * r[0]) % 180).astype(dtype)
-    lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
-    lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
-
-    img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
-    cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
-
-    # Histogram equalization
-    # if random.random() < 0.2:
-    #     for i in range(3):
-    #         img[:, :, i] = cv2.equalizeHist(img[:, :, i])
+#
+# def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
+#     img = img.numpy().astype(np.uint8)
+#     img = img.transpose((1, 2, 0))
+#
+#     r = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
+#     hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+#     # dtype = img.dtype  # uint8
+#     dtype = np.uint8
+#     print(dtype)
+#     x = np.arange(0, 256, dtype=np.int16)
+#     lut_hue = ((x * r[0]) % 180).astype(dtype)
+#     lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+#     lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+#
+#     img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
+#     cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+#
+#
+#     # Histogram equalization
+#     # if random.random() < 0.2:
+#     #     for i in range(3):
+#     #         img[:, :, i] = cv2.equalizeHist(img[:, :, i])
+#     img = img.transpose((2, 0, 1))
+#     img = torch.from_numpy(img)
+#
+#     return img
 
 
 class ImageFolder(Dataset):
@@ -234,12 +246,14 @@ class ListDataset(Dataset):
         if self.augment:
             if np.random.random() < 0.5:
                 img, targets = horisontal_flip(img, targets)
+            # elif np.random.random() < 0.5:
+            #     img = augment_hsv(img)
             elif np.random.random() < 0.5:
                 img, targets = random_affine(img, targets,
                                              # degrees=self.hyp['degrees'],
-                                             translate=self.hyp['translate'],
-                                             scale=self.hyp['scale'],
-                                             shear=self.hyp['shear']
+                                             # translate=self.hyp['translate'],
+                                             # scale=self.hyp['scale'],
+                                             # shear=self.hyp['shear']
                                              )
         return img_path, img, targets
 
